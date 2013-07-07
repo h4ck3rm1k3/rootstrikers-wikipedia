a00c2db00ef4f6f1d20355abd7a1324a7de59d5b
James Michael DuPont <JamesMikeDuPont@gmail.com>
2013-07-07 07:57:57 -0500

    getopt now workss on compare_leg to make it flexible

--- compare_leg.py
+++ compare_leg.py
@@ -2,7 +2,7 @@
 import os
 import legislators_current as leg
 import legislators_other as leg2
-
+import re
 import dump
 import cache 
 
@@ -10,14 +10,57 @@ from cStringIO import StringIO
 legs= leg.load()
 legs2= leg2.load()
 
-def compare(a,b) :
+def compare(a,b,field,convertInt) :
     for x in sorted(a['wp'].keys()):
         aobj = a['wp'][x]
         bobj = b['wp'][x]
-        if 'maplight'  in aobj['id'] :
-            ml = aobj['id']['maplight']
-            bobj['id']['maplight']=ml
-                        
-compare(legs,legs2)
-leg.apply(legs)
-dump.dump(legs2)
+        if field  in aobj['id'] :
+            ml = aobj['id'][field]
+            if convertInt :
+                match= re.search('(\d+)', str(ml))
+                if (match):
+                    ml = match.group(1)
+            b['wp'][x]['id'][field]=ml
+            print "updated", field ,"for id" ,x, ml
+        else :
+            print "missing", field ,"in source" ,x
+
+
+import getopt, sys
+
+def usage():
+    print "--help"
+    print "-i,--int convert value to int"
+    print "-f fieldname,--field=fieldname"
+
+def main():
+    try:
+        opts, args = getopt.getopt(sys.argv[1:], "hif:v", ["help", "field=", "int"])
+    except getopt.GetoptError as err:
+        # print help information and exit:
+        print str(err) # will print something like "option -a not recognized"
+        usage()
+        sys.exit(2)
+    field = None
+    verbose = False
+    convertInt=False
+    for o, a in opts:
+        if o == "-v":
+            verbose = True
+        elif o in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif o in ("-f", "--field"):
+            field = a
+        elif o in ("-i", "--int"):
+            convertInt=True
+        else:
+            assert False, "unhandled option"
+
+    compare(legs,legs2,field,convertInt)
+#    leg.apply(legs)
+    dump.dump(legs2)
+
+
+if __name__ == "__main__":
+    main()
